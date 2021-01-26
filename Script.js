ccfbeaf9c9f75f28a7a5f59a6da8a842039baaf6
Koko est <61022938+MatheusAznavour@users.noreply.github.com>
2021-01-26 02:25:02 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,18 @@
+﻿function checarLogin(){
+    //VARIAVEIS PEGAS
+    let a = document.getElementById('UserImput')
+    a.innerText = 'asuhds'
+
+    //USUARIO 
+    let usuario = document.getElementById('UserMain')   
+    usuario.innerText = a.value
+
+
+    //REFRESH DO SITE
+    const refresh = document.getElementById('Login')
+    if(refresh.parentNode){
+        refresh.parentNode.removeChild(refresh) 
+    }
+    
+}
+
